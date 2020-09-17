b8a749e81050158646309b11e39c16d2ec6a83fd
clark2668 <baclark@msu.edu>
2020-09-17 17:36:10 -0500

    tweak the root run list creation

--- scripts/reped_a5/make_lists/get_A5_root_list.sh
+++ scripts/reped_a5/make_lists/get_A5_root_list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir=/data/user/brianclark/phased_array/A5_data/root_files
+dir=/data/wipac/ARA/2019/blinded/ARA05_PA/ARA05
 
 
 find ${dir}/*/*/event*.root ! -name 'eventHk*.root' > A5_run_list.txt
--- scripts/rootify_a5_data/make_lists/make_io_pairs_and_dirs.sh
+++ scripts/rootify_a5_data/make_lists/make_io_pairs_and_dirs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# make the output directory for each file
+# and also, write a txt file that has the output directory to tar file pairing
+
 input_list=A5_raw_data_unique_tar_list.txt
 final_root_output_location=/data/wipac/ARA/2019/blinded/ARA05_PA/ARA05
 out_file=A5_io_pairs.txt
@@ -7,5 +10,6 @@ out_file=A5_io_pairs.txt
 while read line; do
 	thedir=`dirname $line`
 	themonth=`basename $thedir`
+	mkdir -p $final_root_output_location/$themonth
 	echo $final_root_output_location/$themonth $line >> $out_file
 done < $input_list
